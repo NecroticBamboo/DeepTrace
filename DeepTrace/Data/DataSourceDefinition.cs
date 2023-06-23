6d7f17a34dda614a62eb42691adfc992f71f50b5
Andrey Shabarshov <andrew.shabarshov@gmail.com>
2023-06-23 16:31:01 +0100

    DEEP-9. Datasources management UI implemented.

---
+++ DeepTrace/Data/DataSourceDefinition.cs
@@ -0,0 +1,29 @@
+﻿namespace DeepTrace.Data;
+
+public class DataSourceQuery
+{
+    
+    public DataSourceQuery(string query, string color)
+    {
+        Query = query;
+        Color = color;
+    }
+    
+    public string Query { get; set; }
+    public MudBlazor.Utilities.MudColor Color { get; set; }
+    
+}
+
+public class DataSourceDefinition
+{
+    private static int _instanceId;
+    public DataSourceDefinition()
+    {
+        var id = Interlocked.Increment(ref _instanceId);
+        Name = $"Dataset #{id}";
+    }
+
+    public string Name { get; set; }
+    public List<DataSourceQuery> Queries { get; set; } = new();
+    public string Description { get; set; } = string.Empty;
+}
---
+++ DeepTrace/Data/DataViewHelper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.ML;
+using PrometheusAPI;
+using System.Data;
+
+namespace DeepTrace.Data;
+public static class DataViewHelper
+{
+    public static DataTable? ToDataTable(this IDataView dataView)
+    {
+        DataTable? dt = null;
+        if (dataView != null)
+        {
+            dt = new DataTable();
+            var preview = dataView.Preview();
+            dt.Columns.AddRange(preview.Schema.Select(x => new DataColumn(x.Name)).ToArray());
+            foreach (var row in preview.RowView)
+            {
+                var r = dt.NewRow();
+                foreach (var col in row.Values)
+                {
+                    r[col.Key] = col.Value;
+                }
+                dt.Rows.Add(r);
+
+            }
+        }
+        return dt;
+    }
+
+    public static List<TimeSeries> ToTimeSeries(this IDataView dataView)
+    {
+        var dt = new List<TimeSeries>();
+        if (dataView == null)
+        {
+            return dt;
+        }
+        
+        var preview = dataView.Preview();
+        var tsCol = preview.Schema["TimeStamp"];
+        var valCol = preview.Schema["Value"];
+
+        foreach (var row in preview.RowView)
+        {
+            var r = new TimeSeries(
+                Convert.ToDateTime(row.Values[tsCol.Index].Value),
+                (float)Convert.ToDouble(row.Values[valCol.Index].Value)
+            );
+            dt.Add(r);
+        }
+        return dt;
+    }
+}
